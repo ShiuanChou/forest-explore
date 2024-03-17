a549c4c6ee58997adb626636d91f912f2dfa4efe
Shiuan Chou <10946011@ntub.edu.tw>
2024-03-17 11:36:09 +0800

    modify - favicon

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="zh-Hant">
-      <body suppressHydrationWarning={true}>
+      <body>
         <Navbar />
         {children}
       </body>
