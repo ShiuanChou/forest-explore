4e66bab737d3e9ff34b79261ad34fbbd7c496703
Shiuan Chou <10946011@ntub.edu.tw>
2024-03-17 11:34:58 +0800

    update - VR views's data

---
+++ data/views.js
@@ -0,0 +1,112 @@
+import view1_1 from '@/public/images/view/1-1.jpg';
+import view1_2 from '@/public/images/view/1-2.jpg';
+import view1_3 from '@/public/images/view/1-3.jpg';
+import view1_4 from '@/public/images/view/1-4.jpg';
+import view1_5 from '@/public/images/view/1-5.jpg';
+
+import view2_1 from '@/public/images/view/2-1.jpg';
+import view2_2 from '@/public/images/view/2-2.jpg';
+import view2_3 from '@/public/images/view/2-3.jpg';
+import view2_4 from '@/public/images/view/2-4.jpg';
+import view2_5 from '@/public/images/view/2-5.jpg';
+import view2_6 from '@/public/images/view/2-6.jpg';
+import view2_7 from '@/public/images/view/2-7.jpg';
+import view2_8 from '@/public/images/view/2-8.jpg';
+import view2_9 from '@/public/images/view/2-9.jpg';
+import view2_10 from '@/public/images/view/2-10.jpg';
+import view2_11 from '@/public/images/view/2-11.jpg';
+import view2_12 from '@/public/images/view/2-12.jpg';
+import view2_13 from '@/public/images/view/2-13.jpg';
+import view2_14 from '@/public/images/view/2-14.jpg';
+import view2_15 from '@/public/images/view/2-15.jpg';
+import view2_16 from '@/public/images/view/2-16.jpg';
+import view2_17 from '@/public/images/view/2-17.jpg';
+import view2_18 from '@/public/images/view/2-18.jpg';
+
+import view3_1 from '@/public/images/view/3-1.jpg';
+
+import view4_1 from '@/public/images/view/4-1.jpg';
+
+import view5_1 from '@/public/images/view/5-1.jpg';
+import view5_2 from '@/public/images/view/5-2.jpg';
+import view5_3 from '@/public/images/view/5-3.jpg';
+import view5_4 from '@/public/images/view/5-4.jpg';
+import view5_5 from '@/public/images/view/5-5.jpg';
+
+export const viewsData = [
+	{
+		"id": 1,
+		"location": "東眼山",
+		"images": [view1_1, view1_2, view1_3, view1_4, view1_5],
+		"vrImages": [
+			"https://momento360.com/e/u/dfa1e152e80c4d02b2a5e3917f4e2669?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/f7ebc7d74e8c4e2aa56d8ceb3dffc0f8?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/6dff96b86e4c470588382bd0ec29a560?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/ce64a5453f1a485d8241ea6a734d091d?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/e67784059e374abf929a884e043c464a?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+		],
+		"video": "https://www.youtube.com/embed/A7M7tKmkmwM?si=uSTLos1dXihAijy2"
+	},
+	{
+		"id": 2,
+		"location": "阿里山",
+		"images": [
+			view2_1, view2_2, view2_3, view2_4, view2_5, view2_6,
+			view2_7, view2_8, view2_9, view2_10, view2_11, view2_12,
+			view2_13, view2_14, view2_15, view2_16, view2_17, view2_18
+		],
+		"vrImages": [
+			"https://momento360.com/e/u/11b3003be8294d05ad9c42998e0af460?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/c30da226e8d04f3dac997049559a47de?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/4084c704cc8348d29e59403889de7b53?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/de945eae9fad46928989dc325c088f56?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/b1cf9efb415f462195f7cb94deac6f19?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/044298e27c1c4b02963bac6d49629d6e?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/a5e108c00d9e4d6f9a5cd8e367ef661d?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/b5f9f372275b4cd1acfc779d43a2ed2b?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/eeff955331fd47af80b72135fc813f40?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/9a480e6832904de38f47636b02c56f60?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/a38c9df786d541b9a59d2eab999e1aad?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/412f6fc88f9d42fe9cfb5240719af78c?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/43f5b45f698f4b52accf61707eed9995?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/0ef3b039fa484e4da1d8256e0c7b1c07?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/54ac116793f14c26b04438a29785eb54?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/ec6cb75f71ce4bb1ae2cb631f96edade?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/45b2e2b19362410b9d989729defe0f30?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/46b8bf1aa78d4da1a686f0b3c71c1871?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			
+		],
+		"video": "https://www.youtube.com/embed/9NxRIgmA8SI?si=AA2I_J-JxTdEmYml"
+	},
+	{
+		"id": 3,
+		"location": "望古瀑布",
+		"images": [view3_1],
+		"vrImages": [
+			"https://momento360.com/e/u/6e992c7978764a7cb2bffcf38fdcd3b7?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+		],
+		"video": "https://www.youtube.com/embed/C7tfqUflM1A?si=MhoRP7RA-ilISNzP"
+	},
+	{
+		"id": 4,
+		"location": "處女瀑布",
+		"images": [view4_1],
+		"vrImages": [
+			"https://momento360.com/e/u/56891ce2a4de46ff818c14a3aefc6894?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+		],
+		"video": ""
+	},
+	{
+		"id": 5,
+		"location": "鵝公髻山",
+		"images": [view5_1, view5_2, view5_3, view5_4, view5_5],
+		"vrImages": [
+			"https://momento360.com/e/u/d1943295a3534ac195b22f1db3533ab5?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/42d140b96a6e45b9ae590c085099d050?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/96125f71a2654be99078ade13e4a470a?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/ff6a777b9c384ffba25a4e2b8c7b5730?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+			"https://momento360.com/e/u/aed7f17e2a8a41738a5d461a1e598954?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&autoplay-annotations=true&display-plan=true",
+		],
+		"video": "https://www.youtube.com/embed/stlg_nKQ2Lw?si=cU2AETLjJc5bldJY"
+	}
+]
