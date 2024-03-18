5500eebdefd8466a9d858fe507af13ba19cc9fa8
Shiuan Chou <10946011@ntub.edu.tw>
2024-03-18 16:43:35 +0800

    modify - global's fonts family

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,12 @@
-import "../styles/main.scss"
+import { Noto_Serif_TC } from 'next/font/google';
+import "../styles/main.scss";
 import Navbar from "@/components/Navbar";
+
+const notoSerifTC = Noto_Serif_TC({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400'
+});
 
 export const metadata = {
   title: "護國神殿",
@@ -8,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="zh-Hant">
+    <html lang="zh-Hant" className={notoSerifTC.className}>
       <body>
         <Navbar />
         {children}
