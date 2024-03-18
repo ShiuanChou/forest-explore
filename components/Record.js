b41134ae7aa6c1f6e29987b8e892ea1c2137a602
Shiuan Chou <10946011@ntub.edu.tw>
2024-03-18 16:44:31 +0800

    modify - footer design

--- components/Record.js
+++ components/Record.js
@@ -1,10 +1,21 @@
 import styles from '@/styles/components/record.module.scss';
+import Footer from './Footer';
 
 export default function Record() {
     return (
         <section className={styles.container}>
-            <h1>團隊紀錄片</h1>
-            <video src={require('../public/videos/team.mp4')} width={'50%'} controls autoPlay muted />
+            <div className={styles.content}>
+                <h1>團隊紀錄片</h1>
+                <iframe
+                    width="1120"
+                    height="630"
+                    src="https://www.youtube.com/embed/Zq9ZB7HDLMg?si=hNUyY95kiAXRukNG&amp;autoplay=1"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen></iframe>
+            </div>
+            <Footer />
         </section>
     )
 }
